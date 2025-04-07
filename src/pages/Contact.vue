14dd75e055f24e539d2b78ca4c6dcf31844e36ce
Adrian Pasterka <you@example.com>
2025-04-07 12:31:09 +0200

    Add google map

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,6 +1,8 @@
 <script setup>
 import ContactForm from "@/components/ContactForm.vue";
+import GoogleMap from "@/components/GoogleMap.vue";
 </script>
 <template>
   <ContactForm></ContactForm>
+  <GoogleMap></GoogleMap>
 </template>
