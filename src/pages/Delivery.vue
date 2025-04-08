878bad43c2c538a7b05ad4f5c68fca9dbfa8c1e8
Adrian Pasterka <you@example.com>
2025-04-08 14:32:16 +0200

    New Delivery view layout

--- src/pages/Delivery.vue
+++ src/pages/Delivery.vue
@@ -2,23 +2,25 @@
 import ImageBorder from "@/components/ImageBorder.vue";
 </script>
 <template>
-  <div class="flex flex-col items-center">
-    <ImageBorder class="w-4/5 lg:w-1/3">
+  <div
+    class="!grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] justify-center p-2"
+  >
+    <ImageBorder class="">
       <p>
         Dostawa na terenie wielu krajów Unii Europejskiej: Czechy, Słowacja,
         Austria, Niemcy, Włochy
       </p>
       <img src="/public/assets/ue.avif" class="p-2" alt="" />
     </ImageBorder>
-    <ImageBorder class="w-4/5 lg:w-1/3">
+    <ImageBorder class="">
       <p>Odbiór osobisty</p>
       <img src="/public/assets/c550i.avif" class="p-2" alt="" />
     </ImageBorder>
-    <ImageBorder class="w-4/5 lg:w-1/3">
+    <ImageBorder class="">
       <p>Nasz własny transport</p>
       <img src="/public/assets/transport.avif" class="p-2" alt="" />
     </ImageBorder>
-    <ImageBorder class="w-4/5 lg:w-1/3">
+    <ImageBorder class="">
       <p>Przesyłka kurierska paletowa</p>
       <img src="/public/assets/paleta_edited.avif" class="p-2" alt="" />
     </ImageBorder>
