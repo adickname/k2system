f5917c7ef75fb68844d86e9ee3e295e1539c334a
Adrian Pasterka <you@example.com>
2025-04-08 13:01:02 +0200

    Edit Services view

--- src/pages/Services.vue
+++ src/pages/Services.vue
@@ -1,8 +1,12 @@
 <script setup>
+import ImageBorder from "@/components/ImageBorder.vue";
 import OfferMarquee from "@/components/OfferMarquee.vue";
 </script>
 
 <template>
   <h1><p class="text-center">Tym się zajmujemy:</p></h1>
   <OfferMarquee></OfferMarquee>
+  <ImageBorder class="lg:w-[55%] mx-auto">
+    <img src="/public/assets/services.png" alt="usługi" />
+  </ImageBorder>
 </template>
