710595bc241f9ece2fd3dd9ecd13e66506108e0e
Adrian Pasterka <you@example.com>
2025-04-11 11:12:07 +0200

    Add animations for footer items when moving to another page

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,30 @@
 <script setup>
+import { ref, watch } from "vue";
 import GoogleMap from "./GoogleMap.vue";
+import { useRoute } from "vue-router";
+
+const triggerAnimation = ref(false);
+
+const route = useRoute();
+watch(
+  () => route.name,
+  (newPath, oldPath) => {
+    console.log(5);
+    console.log(newPath);
+    triggerAnimation.value = false;
+    setTimeout(() => {
+      triggerAnimation.value = true;
+    }, 1);
+  }
+);
 </script>
 
 <template>
   <footer>
-    <section class="animate-coming-left">
+    <section :class="{ 'animate-coming-left': triggerAnimation }">
       <GoogleMap class="w-full h-[450px]"></GoogleMap>
     </section>
-    <section class="animate-coming-right">
+    <section :class="{ 'animate-coming-right': triggerAnimation }">
       <div class="bg-black p-2 w-full">
         <p class="text-center">Adres: Spółdzielcza 2, 43-400 Cieszyn</p>
         <p class="text-center">Nip: 5482751226</p>
