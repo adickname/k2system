91107a627a7626f3140a13442e0e2029120aed25
Adrian Pasterka <you@example.com>
2025-04-18 14:21:43 +0200

    Add login system

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/* Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
- */
 
 Route::apiResource('products', ProductController::class);
 
 Route::get('/', function () {
     return 'API';
 });
+
+Route::post('/add-acount', [AuthController::class, 'addAcount']);
+Route::post('/delete-acount', [AuthController::class, 'deleteAcount'])->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
